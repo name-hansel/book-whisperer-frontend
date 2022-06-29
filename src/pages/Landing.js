87be18b2ca498e10180ffbe2a551cfd26956da65
Snehal Mane <snehal6mane@gmail.com>
2022-06-29 20:20:19 +0530

    URL changes on search query change

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
 import React from 'react';
 import debounce from 'lodash.debounce';
+import { useSearchParams } from "react-router-dom";
 
 import SearchResultItem from '../components/SearchResultItem';
 
 const Landing = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [searchQuery, setSearchQuery] = React.useState("");
   const [searchResults, setSearchResults] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
 
+  // Function to get results
   const getResults = async (query) => {
     try {
       const { data } = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/search?query=${query}`);
@@ -18,20 +21,34 @@ const Landing = () => {
     }
   }
 
+  // Debounce API call (getResults)
   const debounceSearch = React.useCallback(debounce(async (query) => {
     await getResults(query);
   }, 100), []);
 
+  // Check url to see if query already exists
+  React.useEffect(() => {
+    if (!searchParams.get("query")) return;
+
+    const existingSearchQuery = decodeURI(searchParams.get("query"));
+    setLoading(true);
+    setSearchQuery(existingSearchQuery);
+    debounceSearch(existingSearchQuery);
+  }, []);
+
+  // Set loading to false when searchResults array changes
   React.useEffect(() => {
     setLoading(false);
   }, [searchResults])
 
+  // Call API when searchQuery changes
   React.useEffect(() => {
     if (!searchQuery.length) {
       setSearchResults([]);
       setLoading(false);
       return;
     }
+    setSearchParams({ query: searchQuery });
     setLoading(true);
     debounceSearch(searchQuery);
   }, [searchQuery]);
