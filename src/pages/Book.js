d409428f0fc6d4b9efd712211edca1cfe486fc90
Snehal Mane <snehal6mane@gmail.com>
2022-07-03 16:23:34 +0530

    Display basic book details

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -34,7 +34,18 @@ const Book = () => {
     <div id="book-div">
       <button id="go-back-btn" onClick={() => navigate(-1)}>&#8249; Go back</button>
       {
-        loading ? <h1>Loading...</h1> : <h1>{goodreadsData.title}</h1>
+        loading ? <h1>Loading...</h1> : <>
+          <div id="book-info-container">
+            <img id="book-cover" src={goodreadsData.imageSource} alt={`Book cover of ${goodreadsData.title}`} />
+            <div id="book-info">
+              <h1 id="book-title">{goodreadsData.title}</h1>
+              <h1 id="book-subtitle">{goodreadsData.subtitle}</h1>
+              <h1 id="book-authors">by {goodreadsData.author.join(", ")}</h1>
+              <h1 id="book-rating">Rating: {goodreadsData.rating}/5</h1>
+              <h1 class="book-number">{`(${goodreadsData.numberOfRatings} ratings, ${goodreadsData.numberOfReviews} reviews)`}</h1>
+            </div>
+          </div>
+        </>
       }
     </div>
   )
